a9160aa2fbe6cf28fb3b344b995ba47a066e1b1a
pepi <f.vega11@gmail.com>
2025-08-29 09:39:52 -0400

    Añadimos menu y recibir opciones

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Programa {
+    public static void main(String[] args) {        menu();    }
 
     public static void menu(){
         int opcion=0;
@@ -11,12 +14,22 @@ public class Programa {
     public static void mostrarMenu(){
         System.out.println("===Menu===");
         System.out.println("Escoja una de las siguiente opciones:");
-        System.out.println("1.");
-        System.out.println("2.");
-        System.out.println("3.");
-        System.out.println("4.");
-        System.out.println("5.");
+        System.out.println("1. Verificar si una frase es revés-derecho");
+        System.out.println("2. Contar cuantas vocales tiene una frase");
+        System.out.println("3. Encriptar");
+        System.out.println("4. Desencriptar");
+        System.out.println("5. Salir");
+    }
 
+    public static int recibirOpcion(){
+         Scanner scan = new Scanner(System.in);
+        int opcionRecibida = 0;
+             do {
+                 try{
+                     opcionRecibida = scan.nextInt();} catch (Exception e){
+                     System.out.println("Ingrese un número válido");}
+             } while ((opcionRecibida > 6) || (opcionRecibida < 1));
+         return opcionRecibida;
     }
 
 }
